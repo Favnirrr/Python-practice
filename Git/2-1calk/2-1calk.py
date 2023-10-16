987a593df904b9cbe8e278527a9173b5f4561260
Favnirrr <nonstop.blocks@gmail.com>
2023-10-16 11:05:13 +0900

    create 2-1calk.py

---
+++ Git/2-1calk/2-1calk.py
@@ -0,0 +1,12 @@
+print( 3 + 5 )      # 和
+print(5-2)          # 差
+print(2*3)          # 積
+print(10/5)         # 商
+print(10//3)        # 商の整数部分
+print(10%3)         # 商の余り
+print(-5)           # 符号反転
+print(2**3)         # べき乗 2^3
+print( abs(-8) )    # 絶対値
+print( int(3.14) )  # 整数化
+print( float(3) )   # 浮動小数点化
+print( round(3.6) ) # 四捨五入

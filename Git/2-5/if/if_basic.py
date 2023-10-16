4069d6e587ed54501ba942812a8fca5d51054de7
Favnirrr <nonstop.blocks@gmail.com>
2023-10-16 12:35:06 +0900

    ✨ create if\if_basic.py

---
+++ Git/2-5/if/if_basic.py
@@ -0,0 +1,18 @@
+# Pythonの条件分岐
+
+# if文の基本
+# if (比較式):
+#   # 比較式が(True)のときの処理
+# else:
+#   # 比較式が(False)のときの処理
+
+print(3 == 3)       # True
+print(3 != 3)       # False
+print(100 > 3)      # True
+print(5 >= 5)       # True
+
+# 変数sに文字列を代入
+s = "pool"
+# 文字列と比較
+print(s == "pool")  # True
+print(s == "bool")  # False

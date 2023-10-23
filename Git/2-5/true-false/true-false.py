962abfc3aa14b1a7e281a286a945cec1dfc111c2
Favnirrr <nonstop.blocks@gmail.com>
2023-10-23 10:22:46 +0900

    create true-false.py

---
+++ Git/2-5/true-false/true-false.py
@@ -0,0 +1,12 @@
+print(11 == 22)     # False
+print(22 == 22)     # True
+
+a = 50
+print(a == 50)      # True
+
+print(True and True)       # True
+print(True and False)      # False
+print(False and False)      # False
+
+a = 40
+print(a > 30 and a < 50)    # True 

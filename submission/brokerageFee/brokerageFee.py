1a79fe5e8380bc610d16371f42eddbfb9ecb35ac
Favnirrr <nonstop.blocks@gmail.com>
2023-11-13 11:24:14 +0900

    🎉 create brokerageFee.py

---
+++ submission/brokerageFee/brokerageFee.py
@@ -0,0 +1,16 @@
+# 売買価格を万円で引数に受け取る
+price = int(input("売買価格を万円で入力: "))
+
+# 仲介手数料を計算
+# 200万円以下のとき 価格*5%
+# 200万円超300万円以下のとき 価格*4%+2万円
+# 300万円超のとき 価格*3%+6万円
+if price <= 200:
+    brokerageFee = price * 0.05
+elif price <= 300:
+    brokerageFee = price * 0.04 +2
+else :
+    brokerageFee = price * 0.03 +6
+
+# 仲介手数料を表示
+print("仲介手数料は", brokerageFee, "万円です。")

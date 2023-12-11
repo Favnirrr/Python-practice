b1c35202709d239d35fb47e06f619707e33feae2
Favnirrr <nonstop.blocks@gmail.com>
2023-12-11 10:47:32 +0900

    ✨ omikuji

--- Git/desktopApp/dialog.py
+++ Git/desktopApp/dialog.py
@@ -1,5 +1,6 @@
 # 208,209,211p
 import tkinter as tk # 208p デスクトップアプリとは 参照
+import random
 
 root = tk.Tk()
 root.geometry("200x100")
@@ -7,7 +8,7 @@ root.geometry("200x100")
 def omikuji():
     omikuji = ["大吉","中吉","小吉","凶"]
     # ランダム
-    import random
+    
     lbl.configure(text=random.choice(omikuji))
 
 lbl =  tk.Label(text="label")

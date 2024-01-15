3271579fba73a34d216969309caf7e0fcdab76f4
Favnirrr <favnirrr224@gmail.com>
2024-01-15 10:47:24 +0900

    edit selimage

--- Git/desktopApp/selimage.py
+++ Git/desktopApp/selimage.py
@@ -4,7 +4,7 @@ import PIL.Image
 import PIL.ImageTk
 
 def dispPhoto(path):
-    newImg = PIL.Image.open(path).convert("L").resize((32,32)).resize((300,300),resample=0)
+    newImg = PIL.Image.open(path).convert("L").resize((8,8)).resize((300,300),resample=0)
     imgData = PIL.ImageTk.PhotoImage(newImg)
     imgLbl.configure(image = imgData)
     imgLbl.image = imgData
@@ -13,15 +13,30 @@ def openFile():
     path = fd.askopenfilename()
     if path:
         dispPhoto(path)
-    
+
+def savePhoto():
+    # 画像を保存する
+    path = fd.asksaveasfilename()
+    if path:
+        imgData.save(path)
+
+def saveFile():
+    path = fd.asksaveasfilename()
+    print(path)
+    if path:
+        savePhoto(path)
+
+imgData = None
 
 root = tk.Tk()
 root.geometry("400x350")
 
-btn = tk.Button(text="ファイルを開く",command=openFile)
+btn_openFile = tk.Button(text="ファイルを開く",command=openFile)
+btn_saveFile = tk.Button(text="画像を保存",command=saveFile)
 imgLbl = tk.Label()
 
-btn.pack()
+btn_openFile.pack()
+btn_saveFile.pack()
 imgLbl.pack()
 
 tk.mainloop()
